9f15e88b26317a8a1a7d505a84cab3a7201f368c
Rastorguev <rastorguevs@gmail.com>
2018-03-31 23:42:05 +0300

    Remove DBProject

--- DBProject/dbo/Tables/Teams.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Teams] (
-    [Id]    INT            IDENTITY (1, 1) NOT NULL,
-    [Name]  NVARCHAR (MAX) NULL,
-    [Coach] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Teams] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
