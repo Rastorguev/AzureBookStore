b6424648c1e5b571560c1bc4065623009f478874
Rastorguev <rastorguevs@gmail.com>
2018-04-11 12:31:43 +0300

    Log unhandled exceptions with lo4net

--- BookStore/Global.asax.cs
+++ BookStore/Global.asax.cs
@@ -1,15 +1,20 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using BookStore.Models;
+using JetBrains.Annotations;
+using log4net;
 using log4net.Config;
 
 namespace BookStore
 {
     public class MvcApplication : HttpApplication
     {
+        [NotNull] private readonly ILog _logger = LogManager.GetLogger(typeof(MvcApplication));
+
         protected void Application_Start()
         {
             Database.SetInitializer(new BookDbInitializer());
@@ -20,6 +25,15 @@ namespace BookStore
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             XmlConfigurator.Configure();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            if (ex != null)
+            {
+                _logger.Fatal(ex.ToString());
+            }
         }
     }
 }
