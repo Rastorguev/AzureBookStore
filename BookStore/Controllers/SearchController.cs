fb5566830079600dd05471ebe4712ec7256c1a1b
Rastorguev <rastorguevs@gmail.com>
2018-05-03 15:51:02 +0300

    Display search results on UI

---
+++ BookStore/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using BookStore.AzureSearch;
+
+namespace BookStore.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly Search _search;
+
+        public SearchController()
+        {
+            _search = new Search();
+        }
+
+        public ActionResult Index(string searchText)
+        {
+            var results = _search.Find(searchText);
+            return View(results);
+        }
+    }
+}
--- BookStore/Controllers/SoccerController.cs
+++ BookStore/Controllers/SoccerController.cs
@@ -1,61 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using  BookStore.Models;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class SoccerController : Controller
     {
-        SoccerContex db = new SoccerContex();
+        private SoccerContex db = new SoccerContex();
 
-		protected override void Dispose(bool disposing)
-		{
-			if (db != null)
-			{
-				db.Dispose();
-				db = null;
-			}
-			base.Dispose(disposing);
-		}
-
-		public ActionResult Filtes(int? team, string position)
+        protected override void Dispose(bool disposing)
         {
-            IQueryable<Player> players = db.Players.Include(p => p.Team);
+            if (db != null)
+            {
+                db.Dispose();
+                db = null;
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Filtes(int? team, string position)
+        {
+            var players = db.Players.Include(p => p.Team);
             if (team != null && team != 0)
             {
                 players = players.Where(p => p.TeamId == team);
             }
-            if (!String.IsNullOrEmpty(position) && !position.Equals("Все"))
+            if (!string.IsNullOrEmpty(position) && !position.Equals("Все"))
             {
                 players = players.Where(p => p.Position == position);
             }
 
-            List<Team> teams = db.Teams.ToList();
+            var teams = db.Teams.ToList();
             // устанавливаем начальный элемент, который позволит выбрать всех
-            teams.Insert(0, new Team { Name = "Все", Id = 0 });
+            teams.Insert(0, new Team {Name = "Все", Id = 0});
 
-	        PlayersListViewModel plvm = new PlayersListViewModel
-	        {
-		        Players = players.ToList(),
-		        Teams = new SelectList(teams, "Id", "Name"),
-		        Positions = new SelectList(new List<string>()
-		        {
-			        "Все",
-			        "Нападающий",
-			        "Полузащитник",
-			        "Защитник",
-			        "Вратарь"
-		        })
-	        };
+            var plvm = new PlayersListViewModel
+            {
+                Players = players.ToList(),
+                Teams = new SelectList(teams, "Id", "Name"),
+                Positions = new SelectList(new List<string>
+                {
+                    "Все",
+                    "Нападающий",
+                    "Полузащитник",
+                    "Защитник",
+                    "Вратарь"
+                })
+            };
             return View(plvm);
         }
-        
-        
-        
+
         // GET: Soccer
         public ActionResult Index()
         {
@@ -65,12 +61,12 @@ namespace BookStore.Controllers
 
         public ActionResult TeamDetails(int? id)
         {
-            id = 2;//?
+            id = 2; //?
             if (id == null)
             {
                 return HttpNotFound();
             }
-            Team team = db.Teams.Find(id);
+            var team = db.Teams.Find(id);
             if (team == null)
             {
                 return HttpNotFound();
@@ -83,7 +79,7 @@ namespace BookStore.Controllers
         public ActionResult Create()
         {
             // Формируем список команд для передачи в представление
-            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            var teams = new SelectList(db.Teams, "Id", "Name");
             ViewBag.Teams = teams;
             return View();
         }
@@ -106,11 +102,11 @@ namespace BookStore.Controllers
                 return HttpNotFound();
             }
             // Находим в бд футболиста
-            Player player = db.Players.Find(id);
+            var player = db.Players.Find(id);
             if (player != null)
             {
                 // Создаем список команд для передачи в представление
-                SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                var teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
                 ViewBag.Teams = teams;
                 return View(player);
             }
